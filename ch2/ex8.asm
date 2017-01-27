51e49672e3327df8954c4772da8428aa2e7266bc
Bobby Durrett <bobby@bobbydurrettdba.com>
2017-01-26 18:12:35 -0700

    Chapter 2

---
+++ ch2/ex8.asm
@@ -0,0 +1,21 @@
+    segment data
+e1a dw 37
+e1b dw 65
+e1c dw 350
+e1d dw 427
+
+e2a dw 0000001010101010b
+e2b dw 1111111111101101b
+e2c dw 0x0101
+e2d dw 0xffcc
+
+e3a dw 0x015a
+e3b dw 0xfedc
+e3c dw 0x0101
+e3d dw 0xacdc
+
+e4a dd 1.375
+e4b dd 0.041015625
+e4c dd -571.3125
+e4d dd 4091.125
+
