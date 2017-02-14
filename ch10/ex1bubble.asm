942b1ff7323297f78057b0d76b825a911d7deb87
Bobby Durrett <bobby@bobbydurrettdba.com>
2017-02-14 15:38:59 -0700

    added malloc

--- ch10/ex1bubble.asm
+++ ch10/ex1bubble.asm
@@ -2,9 +2,11 @@
 ; and sort with bubble sort
        segment .data
 asize  dq 0                     ; array size
+aptr   dq 0                     ; pointer to array
        segment .text
        global main
        extern atol
+       extern malloc
 main:
        push rbp
        mov rbp,rsp
@@ -12,8 +14,13 @@ main:
        cmp rdi,2                ; check for two arguments, second is size
        jl .done
        mov rdi,qword [rsi+8]          ; load pointer to string which is array size number
-       call atol
+       call atol                      ; convert character string to integer.
        mov [asize],rax                ; save array size 
+; Call malloc to allocate the array memory
+       mov rdi,[asize]                ; load array size
+       imul rdi,4                     ; convert to bytes
+       call malloc                    ; allocate memory
+       mov [aptr],rax                 ; store pointer to array
 .done:
        xor eax,eax              ; return code 0
        leave                    ; fix stack
