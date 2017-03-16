5b848abf84f3a179bb74331b187cdaf2eb28c756
Bobby Durrett <bobby@bobbydurrettdba.com>
2017-03-16 08:11:46 -0700

    ch12 ex1 syscall

--- ch12/ex1syscall.asm
+++ ch12/ex1syscall.asm
@@ -15,13 +15,20 @@
 ; xmm0 and xmm1 are the float return resgisters
 ; http://www.nasm.us/links/unix64abi
 
-global main                      
+global main 
+extern atol
+extern malloc
 
 segment .data
 
 from_ptr dq 0
 to_ptr dq 0
 bufsizestrptr dq 0
+buff_size dq 0
+buff_ptr dq 0
+from_fd dq 0
+to_fd dq 0
+bytes_read dq 0
 
 segment .text
 
@@ -29,12 +36,10 @@ main:
     push rbp                     
     mov rbp,rsp                  
 
-; Argc and argv are in rdi and rsi
-; Argc is number of arguments including name of program
-; Argv is array of 8 byte pointers to strings
+; load arguments from command line
 
-    cmp rdi,4                    ; expect 3 arguments plus program name which we ignor
-    jne .exitbadarguments
+    cmp rdi,4                    ; expect 3 arguments plus program name which we ignore
+    jne .exiterror
     mov qword rax, qword [rsi+8]
     mov qword [from_ptr],rax
     mov qword rax, qword [rsi+16]
@@ -42,7 +47,84 @@ main:
     mov qword rax, qword [rsi+24]
     mov qword [bufsizestrptr],rax
     
-.exitbadarguments:
+; allocate buffer for file copy
+
+    mov rdi,rax                  ; move pointer to buf size string to rdi for conversion call
+    call atol                    ; convert buf size as string to number
+    mov qword [buff_size],rax    ; save converted number
+    mov rdi,rax                  ; move buff size to argument for malloc
+    call malloc                  ; allocate memory
+    mov [buff_ptr],rax           ; save pointer to allocated buffer
+    
+; open files
+
+; system call numbers
+; from /usr/include/asm/unistd_64.h:
+; #define __NR_read 0
+; #define __NR_write 1
+; #define __NR_open 2
+; #define __NR_close 3
+
+; from file
+
+    mov rax,2                    ; open file
+    mov rdi,qword [from_ptr]     ; pointer to name of from file
+    xor rsi,rsi                  ; 0 for read only
+    syscall
+    cmp rax,0
+    jl .exiterror
+    mov qword [from_fd],rax      ; save file descriptor for open from file
+    
+; to file
+
+    mov rax,2                    ; open file
+    mov rdi,qword [to_ptr]       ; pointer to name of to file
+    mov rsi,0x41                 ; create file write only
+    mov rdx,644o                 ; create 644
+    syscall
+    cmp rax,0
+    jl .exiterror
+    mov qword [to_fd],rax        ; save file descriptor for open to file
+    
+; copy data from one file to the other
+
+; read a buffer
+
+.nextbuffer:
+    mov rax,0                    ; read from file
+    mov rdi,qword [from_fd]      ; fd of from file
+    mov rsi,qword [buff_ptr]     ; pointer to buffer
+    mov rdx,[buff_size]          ; size of buffer
+    syscall
+    cmp rax,0
+    jl .exiterror
+    je .closefiles
+    mov qword [bytes_read],rax   ; save number of bytes read for write
+
+; write a buffer
+
+    mov rax,1                    ; write to file
+    mov rdi,qword [to_fd]        ; fd of to file
+    mov rsi,qword [buff_ptr]     ; pointer to buffer
+    mov rdx,qword [bytes_read]         ; number of bytes read
+    syscall
+    cmp rax,qword [bytes_read] 
+    jl .exiterror
+    jmp .nextbuffer
+    
+; close the files
+
+.closefiles:    
+    mov rax,3                    ; close file
+    mov rdi,qword [from_fd]      ; fd of from file
+    syscall
+    mov rax,3                    ; close file
+    mov rdi,qword [to_fd]        ; fd of to file
+    syscall
+
+; exit
+
+.exiterror:
     xor rax,rax                  ; return code 0
     leave                        ; fix stack
     ret                          ; return
