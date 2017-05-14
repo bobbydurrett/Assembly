f7154f9aa58ae02fb03fc30d465cf4e93854b780
bobbydurrett <bobby@bobbydurrettdba.com>
2017-05-14 11:48:28 -0400

    hash function

--- ch15/ex3.asm
+++ ch15/ex3.asm
@@ -7,7 +7,7 @@
 ; string of non-space characters a space and a number on input
 ; and just a string of non-space characters on lookup.
 
-global main,read_nonl,insert_hash,query_hash
+global main,read_nonl,insert_hash,query_hash,calc_hash
 extern stdin,fgets,strlen,strcmp,sscanf,printf
 
 segment .bss
@@ -20,6 +20,7 @@ segment .data
 scanfmt db "%s %ld",0
 
 value dq 0                       ; hash table value
+hash_value dq 0                  ; index into hash table
 
 argcntfmt db `Skipped line: %s\n`,0
 
@@ -121,17 +122,64 @@ read_nonl:
     pop rbx
     leave                        ; fix stack
     ret                          ; return
+    
+; insert_hash inserts a value into the hash table using
+; the key. Uses the variables keystr and value as inputs.
 
 insert_hash:	                 
     push rbp                     
-    mov rbp,rsp                  
-
+    mov rbp,rsp  
+    
+    call calc_hash               ; get hash value
+    
     leave                        ; fix stack
     ret                          ; return
 
 query_hash:	                 
     push rbp                     
     mov rbp,rsp                  
+
+    call calc_hash               ; get hash value
+
+    leave                        ; fix stack
+    ret                          ; return
+
+; calc_hash - calculates the hash value based on a string
+; uses the C code in Seyfarth book page 173 but rewritten
+; in assembly.
+
+; Assumes hash table of 100000 members.
+; gets string from keystr
+; push hash result in hash_value
+; register variables
+; rdi - string pointer
+; r8 - hash value
+
+calc_hash:	                 
+    push rbp                     
+    mov rbp,rsp 
+    
+    xor r8,r8                    ; h = 0
+    lea rdi,[keystr]             ; pointer to string
+    
+ .nextbyte:                      ; check next byte of string
+    xor rax,rax                  ; clear rax
+    mov al,byte [rdi]            ; get current byte
+    cmp rax,0                    ; check for 0 byte
+    je .donestring
+    
+    imul r8,191                  ; h*191
+    add r8,rax                   ; add current byte
+    inc rdi                      ; advance to next byte in string
+    jmp .nextbyte
+        
+.donestring:
+
+    xor rdx,rdx                  ; clear rdx for division
+    mov rax,r8                   ; load h for division to get modulus 100000
+    mov r9,100000
+    idiv r9                      ; divide by 100000
+    mov qword [hash_value],rdx   ; save remainder in hash_value
 
     leave                        ; fix stack
     ret                          ; return
